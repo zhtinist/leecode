c3203103edc503aacc04056d95cc27bf311d573a
HTZHU <zhtinist@gmail.com>
2026-07-01 16:53:18 +0800

    archive: move #24 Swap Nodes in Pairs to Linked List/Reversal

--- Linked List/Reversal/24. Swap Nodes in Pairs.py
+++ Linked List/Reversal/24. Swap Nodes in Pairs.py
@@ -39,4 +39,13 @@ from typing import Optional
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if not head or not head.next:
+            return head
+
+        first = head
+        second = head.next
+
+        first.next = self.swapPairs(second.next)
+        second.next = first
+
+        return second
