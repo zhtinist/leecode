626d4a3e368f2e9e1c4a84f5bfcd972b98fc6764
HTZHU <zhtinist@gmail.com>
2026-07-04 23:41:56 +0800

    archive: #53 Maximum Subarray; add: #54 Spiral Matrix

--- Dynamic Programming/1D DP/53. Maximum Subarray.py
+++ Dynamic Programming/1D DP/53. Maximum Subarray.py
@@ -31,4 +31,11 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        current_sum = nums[0]
+        max_sum = nums[0]
+
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
