0e36aabf53704d9cb97456405ca9470acb693fd7
HTZHU <zhtinist@gmail.com>
2026-07-07 00:38:32 +0800

    archive: #55 Jump Game; add: #56 Merge Intervals

--- Array/Greedy/55. Jump Game.py
+++ Array/Greedy/55. Jump Game.py
@@ -29,4 +29,14 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        farthest = 0
+        last = len(nums) - 1
+
+        for i, jump in enumerate(nums):
+            if i > farthest:
+                return False
+            farthest = max(farthest, i + jump)
+            if farthest >= last:
+                return True
+
+        return True
