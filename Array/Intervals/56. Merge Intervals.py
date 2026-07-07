465283e4c1ccef30d831579492416c5d5ce47a59
HTZHU <zhtinist@gmail.com>
2026-07-07 12:30:26 +0800

    archive: #56 Merge Intervals; add: #57 Insert Interval

--- Array/Intervals/56. Merge Intervals.py
+++ Array/Intervals/56. Merge Intervals.py
@@ -28,4 +28,14 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        pass
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            last_end = merged[-1][1]
+            if start <= last_end:
+                merged[-1][1] = max(last_end, end)
+            else:
+                merged.append([start, end])
+
+        return merged
